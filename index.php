f169769b4324028447d709f5f0313455343dd646
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-07 13:49:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-<html>
-<body>
-  
 <?php
-echo "1st PHP Assignment!";
+require_once("header.php");
 ?>
-  
+
+<h1> My PHP website</h1>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
