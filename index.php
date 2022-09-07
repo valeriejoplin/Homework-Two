7f85a5ea4e23b2cab1bb5730afe3f4a8a1334a2a
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-07 14:28:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once("header.php");
   <body>
     <h1>Post response</h1>
     Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+Your email address is: <?php echo $_GET["email"];?>
     <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
 <?php
 require_once("footer.php");
