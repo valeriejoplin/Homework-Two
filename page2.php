b90454bcf6399f321869407493b5a8f83e1857fa
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-07 14:21:20 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -3,7 +3,10 @@ require_once("header.php");
 ?>
 <body style = "text-align:center;">
 <h1> About PHP </h1>
-  <p></p>
+  <p>PHP is a recursive acronym for Hypertext preprocessor and it is a widely used
+  open source general-purpose scripting language. PHP is well designed for web development and can be 
+  embedded into HTML format. PHP contains HTML wth embedded code that does something, it makes working with 
+  HTML easier and requires less repetitive code!</p>
 </body>
 <?php
 require_once("footer.php");
