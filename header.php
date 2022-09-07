33f5e3ce8a7b1bcfabd8431ebb678f6979c11561
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-07 13:59:43 -0500

    Update header.php

--- header.php
+++ header.php
@@ -20,17 +20,16 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">?</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">?</a></li>
+            <li><a class="dropdown-item" href="#">?</a></li>
+            <li><a class="dropdown-item" href="#">?</a></li>
           </ul>
         </li>
       </ul>
