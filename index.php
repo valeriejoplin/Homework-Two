cfe72e6dfc7811d0b39b81ddc78f5fd42782443b
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-09 22:24:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,14 +10,10 @@ require_once("header.php");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <form action="handlepost.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-    
-    <a class="btn btn-primary" href="page2.php" role="button">Submit</a>
+    <h1>Post response</h1>
+    Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+    <a class="btn btn-primary" href="page2.php" role="button">Page 2</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
