adb61f96cbd2fbd03c73ecec1472d179446c4c19
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-12 13:44:19 -0500

    Update header.php

--- header.php
+++ header.php
@@ -20,17 +20,8 @@
           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="page2.php">About PHP</a>
+          <a class="nav-link" href="page3.php">About PHP</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            More
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">?</a></li>
-            <li><a class="dropdown-item" href="#">?</a></li>
-            <li><a class="dropdown-item" href="#">?</a></li>
-          </ul>
         </li>
       </ul>
     </div>
