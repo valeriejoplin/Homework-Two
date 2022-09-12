f49a36680af24215951eedceb6d1615cc6f12b4b
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-12 14:25:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ require_once("header.php");
     <form action="page2.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
+Phone: <input type="text" name="phone"><br>
     <input type="submit">
       </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
