3a1cb5924cdbddd64d378e3ff570bc86124e420e
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-12 13:38:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,12 @@ require_once("header.php");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <form action="handlepost.php" method="get">
+    <h1>Please enter your information</h1>
+    <form action="page2.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 </form>
-    
+    <input type="submit">
     <a class="btn btn-primary" href="page2.php" role="button">Submit</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
