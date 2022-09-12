cc1b2c267133e7d80f27bfa8d88154d366dcef78
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-12 14:21:41 -0500

    Update page3.php

--- page3.php
+++ page3.php
@@ -30,9 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }else if(empty($gname)){
    echo "Please enter your expected graduation date";
   } else {
-    echo $name " ";
-    echo $mname " " ;
-    echo $gname ;
+    echo $name + $mname + $gname;
   }
 }
 ?>
