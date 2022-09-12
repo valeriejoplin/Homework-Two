50e7878af57a8114a6ca0acba69e381cdbb93830
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-12 13:50:40 -0500

    Update page3.php

--- page3.php
+++ page3.php
@@ -1,9 +1,16 @@
 <?php
 require_once("header.php");
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
 <html>
 <body>
-
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
   Name: <input type="text" name="fname">
   <input type="submit">
@@ -23,9 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 </body>
 </html>
-
-
-
+  
 <?php
 require_once("footer.php");
 ?>
