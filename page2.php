a21d07caed88bd9dadfa9d81d26ec60097efd1f9
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-12 13:28:10 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -20,8 +20,8 @@ Your email address is: <?php echo $_GET["email"]; ?>
 <?php
 require_once("footer.php");
 ?>
-<body style = "text-align:center;">
-<!---<h1> About PHP </h1>
+<!---<body style = "text-align:center;">
+<h1> About PHP </h1>
   <p>PHP is a recursive acronym for Hypertext preprocessor and it is a widely used
   open source general-purpose scripting language. PHP is well designed for web development and can be 
   embedded into HTML format. PHP contains HTML wth embedded code that does something, it makes working with 
