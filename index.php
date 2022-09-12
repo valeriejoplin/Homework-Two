e777d2d8fa4a0376cc24b122a4d5283d046c24a8
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-12 13:42:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,6 @@ Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
     <input type="submit">
       </form>
-  <---<a class="btn btn-primary" href="page2.php" role="button">Submit</a>--->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
