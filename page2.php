48cf93890a542dc08565ddef1a8f3a2d4d6c2bcb
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-12 14:26:17 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -13,6 +13,8 @@ require_once("header.php");
     <h1>Post response</h1>
     Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
+Your phone number is: <?php echo $_GET["phone"]; ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
