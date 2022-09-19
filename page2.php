5180afbce5022ee50448983e8b568da1a3e40f5f
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-19 10:16:22 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -21,11 +21,4 @@ Your phone number is: <?php echo $_GET["phone"]; ?><br>
 <?php
 require_once("footer.php");
 ?>
-<!---<body style = "text-align:center;">
-<h1> About PHP </h1>
-  <p>PHP is a recursive acronym for Hypertext preprocessor and it is a widely used
-  open source general-purpose scripting language. PHP is well designed for web development and can be 
-  embedded into HTML format. PHP contains HTML wth embedded code that does something, it makes working with 
-  HTML easier and requires less repetitive code!</p>
-</body>
---->
+
