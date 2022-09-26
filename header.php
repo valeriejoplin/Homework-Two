9878967a042bdb1c04c7b5e49b227aa20eceb3e9
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 15:33:01 -0500

    Update header.php

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Personal Information</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="page3.php">Student Information</a>
