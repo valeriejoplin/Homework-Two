58fc20b08ff4f82bd62741365b1473fe2742c1a0
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 14:24:52 -0500

    Update page4.php

--- page4.php
+++ page4.php
@@ -4,7 +4,12 @@ require_once("header.php");
 <body>
 <?php 
 function DateFunction(){
-    echo (new \DateTimeNow())->format('Y-m-d H:i:s');
+     $tz_object = new DateTimeZone('Chicago/America');
+ 
+
+    $datetime = new DateTime();
+    $datetime->setTimezone($tz_object);
+    return $datetime->format('Y\-m\-d\ h:i:s');
 }
   echo DateFunction()
  ?>
