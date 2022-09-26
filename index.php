f4cc9b03502c1afe65d6e757d8ea17d11b9b9ae8
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 13:53:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,6 @@ E-mail: <input type="text" name="email"><br>
 Phone: <input type="text" name="phone"><br>
     <input type="submit">
       </form>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-</html>
 <?php
 require_once("footer.php");
 ?>
