7d77795da29b1ccf5f91baef7c06f5861769b62f
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 14:14:20 -0500

    Update page4.php

--- page4.php
+++ page4.php
@@ -3,19 +3,11 @@ require_once("header.php");
 ?>
 <body>
 <?php 
-function TimeFunction() {
-date_default_timezone_set("America/Chicago");
-echo "The time is " .
-date("h:i:sa");
-}
 function DateFunction(){
     $dt = new DateTime();
     echo $dt->format('Y-m-d H:i:s');
 }
- 
- echo TimeFunction()
   echo DateFunction()
-  
  ?>
  
 </body>
