1971d669b7486265d4f3c1f648cb8745b7d55a25
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 13:33:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
 require_once("header.php");
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
   <body>
     <h3>Please enter your information</h3>
     <form action="page2.php" method="get">
