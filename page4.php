ac39fa5f0578f1824c02e56fd3ffa92966f5270a
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 13:59:35 -0500

    Update page4.php

--- page4.php
+++ page4.php
@@ -4,7 +4,7 @@ require_once("header.php");
 <body>
 <?php 
 function FirstFunction() {
- echo Welcome! Hope you are enjoying learning about PHP!;
+ echo Welcome! Hope you are enjoying learning about PHP!
 }
  echo FirstFunction()
  ?>
