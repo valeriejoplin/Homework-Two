4655000011aaf8f2559cc9608edba2a0b37eed93
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 15:44:45 -0500

    Update page3.php

--- page3.php
+++ page3.php
@@ -11,19 +11,19 @@ require_once("header.php");
   <input type="submit">
 </form>
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST['fname'];
-  $mname = $_POST['major'];
+  $maame = $_POST['major'];
+  $miname = $_POST['minor'];
   $gname = $_POST['gradyear'];
-  if (empty($name)) {
+  if (empty($maname)) {
     echo "Please enter your name";
-  }else if(empty($mname)){
+  }else if(empty($miname)){
    echo "Please enter your major";
   }else if(empty($gname)){
    echo "Please enter your expected graduation date";
   } else {
      function FunctionOutput(){
-    echo $name; .
     echo $maname; .
+    echo $miname; .
     echo $gname; 
 }
   call FunctionOutput()
