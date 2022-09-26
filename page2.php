887bb0227308b70433cfbf56332c305eca411219
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 15:58:34 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -2,7 +2,7 @@
 require_once("header.php");
 ?>
   <body>
-    <h2>Response Completed</h2>
+    <h2>Response Received</h2>
     Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?><br>
 Your phone number is: <?php echo $_GET["phone"]; ?><br>
