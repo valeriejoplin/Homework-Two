bd26e01940a090f53d2eb1c6fb6d7810234b4699
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 14:17:33 -0500

    Update page4.php

--- page4.php
+++ page4.php
@@ -6,7 +6,7 @@ require_once("header.php");
 function DateFunction(){
     $dt = new DateTime();
     echo "The current date and time are " . 
-        $dt->format('Y-m-d H:i:s');
+        $dt->format('D-m-y H:i:s');
 }
   echo DateFunction()
  ?>
