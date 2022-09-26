915c85eee8ae154c3299661dfb1a00913aef7111
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 15:49:57 -0500

    Update page3.php

--- page3.php
+++ page3.php
@@ -3,27 +3,28 @@ require_once("header.php");
 ?>
 <html>
 <body>
-  <h3> Fill out your student information below </h3>
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="major">
-  Major: <input type="text" name="minor">
+  Name: <input type="text" name="fname">
+  Major: <input type="text" name="major">
   Graduation Date: <input type ="text" name="gradyear">
   <input type="submit">
 </form>
+
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $major = $_POST['major'];
-  $minor = $_POST['minor'];
-  $gradyr = $_POST['gradyear'];
-  if (empty($major)) {
-    echo "Please enter your major";
-  }else if(empty($minor)){
-   echo "Please enter your minor, if none, enter "None" ";
-  }else if(empty($gradyr)){
+  $name = $_POST['fname'];
+  $mname = $_POST['major'];
+  $gname = $_POST['gradyear'];
+  if (empty($name)) {
+    echo "Please enter your name";
+  }else if(empty($mname)){
+   echo "Please enter your major";
+  }else if(empty($gname)){
    echo "Please enter your expected graduation date";
-  } else 
-    echo $major;
-    echo $minor;
-    echo $gradyr; 
+  } else {
+    echo $name;
+    echo $mname;
+    echo $gname;
   }
 }
 ?>
