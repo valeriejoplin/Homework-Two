7bbd1801a61c98bdffc019ae51b5f564a29be938
Valerie Joplin <89474266+valeriejoplin@users.noreply.github.com>
2022-09-26 14:29:46 -0500

    Update page4.php

--- page4.php
+++ page4.php
@@ -4,13 +4,13 @@ require_once("header.php");
 <body>
 <?php 
 function DateFunction(){
-        date.timezone = "America/Chicago";
+  date_default_timezone_set("America/Chicago");
   $currentDateTime = date('m-d-Y H:i:s');
     echo $currentDateTime;
-
 }
   echo DateFunction()
  ?>
  
 </body>
 </html>
+
